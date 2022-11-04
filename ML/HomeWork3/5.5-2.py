14cfa4acbe620a74bc52f8b39a7dd9f98742e6df
yuxuan <2309941940@qq.com>
2022-11-04 17:25:40 +0800

    3-1 ok with good acc

--- ML/HomeWork3/5.5-2.py
+++ ML/HomeWork3/5.5-2.py
@@ -14,14 +14,11 @@ def par_sigmoid(x):
 class ANN:
     input_num,hidden_num,output_num,learning_rate,epochs=100,100,100,0.1,200
     y,beta,w,b,alpha,v,x=[],[],[],[],[],[],[]
-    theta,gamma,hat_y,e=[],[],[],[]
-    loss,wg=[],[]
+    theta,gamma,hat_y,e,g=[],[],[],[],[]
     # def g(self,k):
     #     return self.hat_y[k]*(1-self.hat_y[k])*(self.y[k]-self.hat_y[k])
 
-    def __init__(self,x,y,learning_rate,input_num,hidden_num,output_num):
-        self.x=x #x should be (num,d)
-        self.y=y #y should be (1,l)
+    def __init__(self,learning_rate,input_num,hidden_num,output_num):
         self.learning_rate=learning_rate
         self.input_num=input_num
         self.hidden_num=hidden_num
@@ -30,49 +27,58 @@ class ANN:
         #阈值的初始化
         self.theta=np.random.rand(output_num)
         self.gamma=np.random.rand(hidden_num)
-
         self.hat_y=np.zeros(output_num)
         #权重初始化
         self.v=np.zeros((input_num,hidden_num))
         self.w=np.zeros((hidden_num,output_num))
-        self.e=np.zeros((1,hidden_num))
+        self.e=np.zeros(hidden_num)
+        self.alpha=np.zeros(hidden_num)
+        self.beta=np.zeros(output_num)
+        self.g=np.zeros(output_num)
+    def get_y(self,x):
+        for h in range(self.hidden_num):
+            self.alpha[h]=0
+            for i in range(self.input_num):
+                self.alpha[h]+=self.v[i][h]*x[i]
+            self.b[h]=sigmoid(self.alpha[h]-self.gamma[h])
+        for o in range(self.output_num):
+            self.beta[o]=0
+            for h in range(self.hidden_num):
+                self.beta[o]+=self.w[h][o]*self.b[h]
+            self.hat_y[o]=sigmoid(self.beta[o]-self.theta[o])
     def train_std(self,x1,y1):
-        loss=0
         #前向传播
-        x1,y1=x1.reshape(-1,1),y1.reshape(-1,1)
-        self.alpha= np.dot(x1.T,self.v)
-        self.b= sigmoid(self.alpha-self.gamma)
-        self.beta= np.dot(self.b,self.w)
-        self.hat_y= sigmoid(self.beta-self.theta)
-        ##
+        self.get_y(x1)
         #求得E均方误差
         E=0
-        for i in range(len(y1)):
-            #print(self.hat_y[i]  )
-            E+=(self.hat_y[0,i]-y1[i])**2
+        for i in range(len(y1[0])):
+            E+=(self.hat_y[i]-y1[i])**2
         E/=2
         loss=E
+        #print(loss)
         #反向传播
-        #g=np.multiply(self.hat_y,1-self.hat_y,self.y[j]-self.hat_y)
-        g=np.zeros((1,self.output_num))
+        #get g and e
         for i in range(self.output_num):
-            g[0,i]= self.hat_y[0,i]*(1-self.hat_y[0,i])*(y1[i]-self.hat_y[0,i])
-        wg=np.dot(self.w,g.T)
-        for k in range(self.hidden_num):
-            self.e[0,k]=self.b[0,k]*(1-self.b[0,k])*wg[k,0]
-
-        self.w+=self.learning_rate*np.dot(self.b.T,g)
-
+            self.g[i]=(self.hat_y[i]*(1-self.hat_y[i])*(y1[i]-self.hat_y[i]))
+        for h in range(self.hidden_num):
+            tmp=0
+            for o in range(self.output_num):
+                tmp+=self.w[h][o]*self.g[o]
+            self.e[h]=self.b[h]*(1-self.b[h])*tmp
+        for h in range(self.hidden_num):
+            for o in range(self.output_num):
+                self.w[h][o]+=self.learning_rate*self.g[o]*self.b[h]
+        for i in range(self.input_num):
+            for h in range(self.hidden_num):
+                self.v[i][h]+=self.learning_rate*x1[i]*self.e[h]
+        #self.w+=self.learning_rate*np.dot(self.b.T,g)
         for i in range(self.output_num):
-            self.theta[i]+=-self.learning_rate*g[0,i]
-        self.v+=self.learning_rate*np.dot(x1,self.e)
+            self.theta[i]-=self.learning_rate*self.g[i]
+        #self.v+=self.learning_rate*np.dot(x1,self.e)
         for i in range(self.hidden_num):
-            self.gamma[i]+=-self.learning_rate*self.e[0,i]
+            self.gamma[i]-=self.learning_rate*self.e[i]
     def predict(self,x1):
-        self.alpha = np.dot(x1, self.v)
-        self.b = sigmoid(self.alpha - self.gamma)
-        self.beta = np.dot(self.b, self.w)
-        self.hat_y = sigmoid(self.beta - self.theta)
+        self.get_y(x1)
         return self.hat_y
 def read_iris():
     with open("E:/desktop/3rdgrade-1/机器学习/IRIS/iris.csv", "r") as file:#打开CSV文件
@@ -108,7 +114,7 @@ enc=preprocessing.LabelEncoder()
 label=enc.fit_transform(label)
 s=set()
 for i in label:s.add(i)
-input,hidden,output=len(img[0]),100,len(s)#设置输入层，隐藏层，输出层的神经元个数 125best now
+input,hidden,output=len(img[0]),125,len(s)#设置输入层，隐藏层，输出层的神经元个数 125best now
 #print(test_img,test_img)
 mmx=MinMaxScaler()#输入数据归一化
 tot_img,tot_label=np.matrix(img),np.matrix(label)
@@ -121,7 +127,7 @@ tot_img,tot_label=tot[:,:input],tot[:,input:]#归一化以后的数据和tag
 train_x,test_x,train_y,test_y=sklearn.model_selection.train_test_split(tot_img,tot_label,train_size=0.8)#抽取80%作为训练集
 ################使用手工搭建的神经网络##################################
 learning_rate=0.2#确定学习率
-ANN1=ANN(x=tot_img,y=tot_label,learning_rate=0.2,input_num=input,hidden_num=hidden,output_num=output)
+ANN1=ANN(learning_rate=0.1,input_num=input,hidden_num=hidden,output_num=output)
 epochs=200
 ANN_res,Acc_res=ANN1,0
 for k in range(epochs):#迭代
