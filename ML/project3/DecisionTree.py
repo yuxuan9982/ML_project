47eb66069afb920733808e507b62214355764242
yuxuan <2309941940@qq.com>
2022-11-16 14:18:00 +0800

    project 3 continuous success

--- ML/project3/DecisionTree.py
+++ ML/project3/DecisionTree.py
@@ -1,6 +1,9 @@
+import csv
 from math import log, log2
+from random import shuffle
 
 import xlrd
+from sklearn.model_selection import train_test_split
 
 
 def get_ant(data):
@@ -24,12 +27,43 @@ def gain(data,a):
         gain-= posi*get_ant(n_data)
     return gain
 
+
+def handle_continuous(data,a):
+    candidate,lst=[],[sample[a] for sample in data]
+    for i in range(1,len(lst)):
+        candidate.append((lst[i]+lst[i-1])/2)
+    best_ent,best_candi,base_ent=-0x3f3f3f3f,0,get_ant(data)
+    for candi in candidate:
+        l_data,r_data=[],[]
+        for i in data:
+            if i[a]>=candi:
+                r_data.append(i)
+            else:l_data.append(i)
+        ent1,posi1=get_ant(l_data),len(l_data)/len(data)
+        ent2,posi2=get_ant(r_data),len(r_data)/len(data)
+        tmp_ent= base_ent-posi1*ent1-posi2*ent2
+        if tmp_ent >=best_ent:
+            best_ent=tmp_ent
+            best_candi=candi
+    return best_ent,best_candi
+
+
 def find_best(data):
-    best_ent,best_fea=0,0
+    best_ent,best_fea=-0x3f3f3f3f,0
     for i in range(len(data[0])-1):
+        if type(data[0][i]).__name__=='float':continue
         if gain(data,i) > best_ent:
             best_ent,best_fea=gain(data,i),i
-    return best_fea
+    return best_ent,best_fea
+
+def find_best_continuous(data):
+    best_ent, best_fea,best_val = -0x3f3f3f3f,0,0
+    for i in range(len(data[0]) - 1):
+        if type(data[0][i]).__name__ != 'float': continue
+        candi_ent,candi_val=handle_continuous(data,i)
+        if candi_ent > best_ent:
+            best_ent, best_fea,best_val = candi_ent, i,candi_val
+    return best_ent, best_fea,best_val
 
 def majority(label):
     cnt={}
@@ -49,22 +83,40 @@ def dfs(data,name):
         return label[0]
     if len(data[0])==1:
         return majority(label)
-    best = find_best(data)
+
+    best_ent,best = find_best(data)
+    best_ent2,best2,best2_val=find_best_continuous(data)
+
+    #print(best_ent2,best2,best2_val)
     #print(data[0],best,name)
-    best_label = name[best]
-    Tree ={best_label:{}}
-    del(name[best])
-    dif_fea=set([i[best] for i in data])
-    for fea in dif_fea:
-        n_data,n_name= [],name[:]#without n_name origin name will be changed
+    if best_ent2<best_ent:
+        best_label = name[best]
+        Tree ={best_label:{}}
+        del(name[best])
+        dif_fea=set([i[best] for i in data])
+        for fea in dif_fea:
+            n_data,n_name= [],name[:]#without n_name origin name will be changed
+            for i in data:
+                if (i[best] == fea):
+                    tmp=[]
+                    for j in range(len(i)):
+                        if j!=best:tmp.append(i[j])
+                    n_data.append(tmp)
+            Tree[best_label][fea]=dfs(n_data,n_name)
+        return Tree
+    else:
+        best_label=name[best2]
+        Tree={best_label:{}}
+        n_data,n_name,n_data2,n_name2=[],name[:],[],name[:]
         for i in data:
-            if (i[best] == fea):
-                tmp=[]
-                for j in range(len(i)):
-                    if j!=best:tmp.append(i[j])
-                n_data.append(tmp)
-        Tree[best_label][fea]=dfs(n_data,n_name)
-    return Tree
+            if i[best2]<best2_val:
+                n_data.append(i)
+            else: n_data2.append(i)
+        Tree[best_label]["<"+str(best2_val)]=dfs(n_data,n_name)
+        Tree[best_label][">="+str(best2_val)]=dfs(n_data2,n_name2)
+        return Tree
+
+
 
 def predict(Tree,name,test):
     feature,key= 0,Tree.keys()
@@ -74,6 +126,17 @@ def predict(Tree,name,test):
     dic2= Tree[feature]
     #print(dic2)
     index= name.index(feature)
+    if(type(test[index]).__name__=='float'):
+        val= list(dic2.keys())[0]
+        val = float(val[1:])
+        if(test[index]<val):
+            res='<'+str(val)
+        else:
+            res='>='+str(val)
+        if(type(dic2[res]).__name__=='dict'):
+            return predict(dic2[res],name,test)
+        else:
+            return dic2[res]
     for key in dic2.keys():
         if test[index]==key:
             if(type(dic2[key]).__name__=='dict'):
@@ -99,12 +162,42 @@ def get_water():
     # print(data);print(label)
     return data, label
 
-data,label=get_water()
+def read_iris():
+    with open("E:/desktop/3rdgrade-1/机器学习/IRIS/iris.csv", "r") as file:#打开CSV文件
+        reader = csv.reader(file)#读取
+        lst = list(reader)#获取list
+    label=lst[0][1:]
+    lst.pop(0)#第一行不要
+    data=[]
+    for i in lst:
+        for j in range(1, 5):
+            i[j] = float(i[j])#转换为浮点类型
+        data.append(i[1:])#data增加
+    #print(data)
+    #print(label)
+    return data,label
+    # for i in lst:print(i)
+
+
+
+
+data,label=read_iris()
 #print(data)
 #print(label)
+###divide
+shuffle(data)
+train,test=[],[]
+for i in range(len(data)):
+    if(i<len(data)*0.8):
+        train.append(data[i])
+    else:
+        test.append(data[i])
+###devide
 lab2=label[:]
-Tree = dfs(data,lab2)
-#print(Tree)
-a=0
-for d in data:
-    print(predict(Tree,label,d))
+Tree = dfs(train,lab2)
+print(Tree)
+cnt=0
+for d in test:
+    if (predict(Tree,label,d[:-1])==d[-1]):
+        cnt+=1
+print('accuracy:',cnt/len(test))
